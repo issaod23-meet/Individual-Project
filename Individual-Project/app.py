07e8bf97ce59bd6692e799ae661f5ddc3d81046b
issaod23-meet <109899312+issaod23-meet@users.noreply.github.com>
2022-08-03 17:00:23 +0300

    day2 work

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
@@ -6,21 +7,19 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 #Code goes below here
-
 Config = {
-  "apiKey": "AIzaSyDLiUWZo_ozK7uwE5L9wZqJ84bnoBFvt_E",
-  "authDomain": "individual-csproject-y2-summer.firebaseapp.com",
-  "databaseURL": "https://individual-csproject-y2-summer-default-rtdb.europe-west1.firebasedatabase.app",
-  "projectId": "individual-csproject-y2-summer",
-  "storageBucket": "individual-csproject-y2-summer.appspot.com",
-  "messagingSenderId": "635400044982",
-  "appId": "1:635400044982:web:46634681b877eb97d90117",
-  "measurementId": "G-ZJ4QHSE2CM",
-  "databaseURL":"https://individual-csproject-y2-summer-default-rtdb.europe-west1.firebasedatabase.app"
+  'apiKey': "AIzaSyDLiUWZo_ozK7uwE5L9wZqJ84bnoBFvt_E",
+  'authDomain': "individual-csproject-y2-summer.firebaseapp.com",
+  'databaseURL': "https://individual-csproject-y2-summer-default-rtdb.europe-west1.firebasedatabase.app",
+  'projectId': "individual-csproject-y2-summer",
+  'storageBucket': "individual-csproject-y2-summer.appspot.com",
+  'messagingSenderId': "635400044982",
+  'appId': "1:635400044982:web:46634681b877eb97d90117",
+  'measurementId': "G-ZJ4QHSE2CM", 'databaseURL':'https://individual-csproject-y2-summer-default-rtdb.europe-west1.firebasedatabase.app/'
   }
 
 firebase= pyrebase.initialize_app(Config)
-auth = firebase.auth()
+auth = firebase.auth()  
 db = firebase.database()
 
 @app.route('/', methods=['GET', 'POST'])
@@ -30,12 +29,62 @@ def home():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+       email = request.form['email']
+       password = request.form['password']
+    
+       login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+       return redirect(url_for('food_page'))
+
     return render_template("Login.html")
 
 
-@app.route('/signup', methods=['GET', 'POST'])
+
+@app.route('/Signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        full_name = request.form['full_name']
+        # address = request.form['address']
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            user = {"full_name": full_name, "email": email}
+            db.child("Users").child(login_session['user']['localId']).set(user)
+            return redirect(url_for('food_page'))
+        except:
+            error = "Authentication failed"
     return render_template("Signup.html")
+
+
+@app.route('/food_page', methods=['GET', 'POST'])
+def food_page():
+    return render_template("Food_page.html")
+
+@app.route('/add_review', methods=['GET', 'POST'])
+def food_add():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        review = {"name":name,"email":email,"message":message}
+        db.child("Reviews").push(review)
+        x = db.child("Reviews").get().val()
+
+
+    return render_template("reviews.html", y = x)
+
+
+
+
+@app.route('/reviews', methods=['GET', 'POST'])
+def reviews():
+   
+    x = db.child("Reviews").get().val()
+
+
+    return render_template("reviews.html", y = x)
 
 
 
